c1f622e03a449e6a62b7e3573fde9f4142077b74
tachymetre <vince.pham.149@gmail.com>
2016-04-05 10:15:12 -0400

    add in client codes in Angular

---
+++ client/src/scripts/app.js
@@ -0,0 +1,73 @@
+/**
+ *
+ * app.js
+ * Including vendor libraries & template scripting functions
+ *
+ **/
+/*==================================
+=            APP CONFIG            =
+==================================*/
+'use strict';
+// Require other components to be bundled together
+require('./controllers');
+
+// Create app settings and routing
+var app = angular.module("pantoum", ['ui.router', 'satellizer', 'permission']);
+app.config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    $authProvider.loginUrl = 'http://pantoum.dev/api/v1/authenticate';
+    $urlRouterProvider.otherwise('/auth');
+    $stateProvider.state('auth', {
+        url: '/auth',
+        data: {
+            permissions: {
+                except: ['isLoggedIn'],
+                redirectTo: 'blogs'
+            }
+        },
+        views: {
+            'blogsContent': {
+                templateUrl: 'views/auth.html',
+                controller: 'AuthCtrl as auth'
+            }
+        }
+    }).state('blogs', {
+        url: '/blogs',
+        data: {
+            permissions: {
+                except: ['anonymous'],
+                redirectTo: 'auth'
+            }
+        },
+        views: {
+            'blogsContent': {
+                templateUrl: 'views/blogs.html',
+                controller: 'BlogsCtrl as blogs'
+            }
+        }
+    });
+})
+
+// Execute logout logics and permission settings
+app.run(function($rootScope, $state, $auth, PermissionStore) {
+    $rootScope.logout = function() {
+        $auth.logout().then(function() {
+            localStorage.removeItem('user');
+            $rootScope.currentUser = null;
+            $state.go('auth');
+        });
+    }
+    $rootScope.currentUser = JSON.parse(localStorage.getItem('user'));
+    PermissionStore.definePermission('isLoggedIn', function(stateParams) {
+        // If the returned value is *truthy* then the user has the role, otherwise they don't
+        if ($auth.isAuthenticated()) {
+            return true;
+        }
+        return false;
+    });
+    PermissionStore.definePermission('anonymous', function(stateParams) {
+        if (!$auth.isAuthenticated()) {
+            return true;
+        }
+        return false;
+    });
+});
---
+++ client/src/scripts/controllers/auth.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = function($scope, $auth, $state, $http) {
+    var vm = this;
+    vm.loginError = false;
+    vm.loginErrorText;
+    vm.login = function() {
+        var credentials = {
+            email: vm.email,
+            password: vm.password
+        };
+
+        $auth.login(credentials).then(function() {
+            $http.get('http://pantoum.dev/api/v1/authenticate/user')
+                .success(function(response) {
+                    var user = JSON.stringify(response.user);
+                    localStorage.setItem('user', user);
+                    $scope.currentUser = response.user;
+                    $state.go('blogs');
+                })
+                .error(function(error) {
+                    vm.loginError = true;
+                    vm.loginErrorText = error.data.error;
+                    console.log(vm.loginErrorText);
+                })
+        });
+    }
+}
