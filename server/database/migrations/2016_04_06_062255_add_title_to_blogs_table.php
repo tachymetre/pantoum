557a87cfa00c247f5e85c07dd725d6651ee00110
tachymetre <vince.pham.149@gmail.com>
2016-04-06 02:33:03 -0400

    add in new migration for adding a column into blogs table

---
+++ server/database/migrations/2016_04_06_062255_add_title_to_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTitleToBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('title');
+        });
+    }
+}
