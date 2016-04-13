2ac4a0fb86d0026e64a2fb7095bdf2bf4d6b13db
tachymetre <vince.pham.149@gmail.com>
2016-04-13 01:05:39 -0400

    add in implementation for loading more contents when scrolling down

--- client/src/scripts/directives/loadMoreContent.js
+++ client/src/scripts/directives/loadMoreContent.js
@@ -1,18 +1,65 @@
 'use strict';
-module.exports = function($document) {
+module.exports = function() {
     return {
         restrict: 'E',
         link: function(scope, elem, attrs) {
+            // Calculate current time to detect a shift in calling 
+            var timeNow = Date.now || function() {
+                return new Date().getTime();
+            };
+            // Throttle funtion to rate-limit loading based on scrolling events
+            function throttle(func, wait, options) {
+                var timeout, context, args, result;
+                var previous = 0;
+                if (!options) options = {};
+
+                var later = function() {
+                    previous = options.leading === false ? 0 : timeNow();
+                    timeout = null;
+                    result = func.apply(context, args);
+                    if (!timeout) context = args = null;
+                };
+
+                var throttled = function() {
+                    var now = timeNow();
+                    if (!previous && options.leading === false) previous = now;
+                    var remaining = wait - (now - previous);
+                    context = this;
+                    args = arguments;
+                    if (remaining <= 0 || remaining > wait) {
+                        if (timeout) {
+                            clearTimeout(timeout);
+                            timeout = null;
+                        }
+                        previous = now;
+                        result = func.apply(context, args);
+                        if (!timeout) context = args = null;
+                    } else if (!timeout && options.trailing !== false) {
+                        timeout = setTimeout(later, remaining);
+                    }
+                    return result;
+                };
+
+                throttled.cancel = function() {
+                    clearTimeout(timeout);
+                    previous = 0;
+                    timeout = context = args = null;
+                };
+
+                return throttled;
+            };
+            // Implement loading function when scrolling to bottom
             var lastScroll = 0;
-            $(window).scroll(function(e) {
-                var st = $(this).scrollTop();
-                console.log(st);
-                if (st > lastScroll && st >= $(document).height() - $(window).height() - 100) {
-                    console.log("Fire");
+            $(window).scroll(throttle(function() {
+                var windowTop = $(this).scrollTop(),
+                    windowHeight = $(this).height(),
+                    documentHeight = $(document).height();
+
+                if (windowTop > lastScroll && windowTop >= documentHeight - windowHeight - 300) {
                     scope.$apply(attrs.initLoad);
                 }
-                lastScroll = st;
-            });
+                lastScroll = windowTop;
+            }, 300));
         }
     }
 }
