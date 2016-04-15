7cdcca3c539526373dbb0c47445ed99de17f01ae
tachymetre <vince.pham.149@gmail.com>
2016-04-14 21:07:58 -0400

    redefine the scrollHide and fine tuning the functionality

--- client/src/scripts/directives/scrollHide.js
+++ client/src/scripts/directives/scrollHide.js
@@ -3,10 +3,10 @@ module.exports = function() {
     return {
         restrict: 'A',
         link: function(scope, elem, attrs) {
-            var didScroll;
-            var lastScrollTop = 0;
-            var delta = 5;
-            var navbarHeight = $(elem).outerHeight();
+            var didScroll,
+                lastScrollTop = 0,
+                delta = 5,
+                navbarHeight = $(elem).outerHeight();
 
             $(window).scroll(function(e) {
                 didScroll = true;
@@ -20,13 +20,15 @@ module.exports = function() {
             }, 250);
 
             function hasScrolled() {
-                var windowTop = $(window).scrollTop();
-                if (Math.abs(lastScrollTop - windowTop) <= delta)
+                var windowTop = $(window).scrollTop(),
+                    $dropDownsIsActive = $('.pa-dropdown-wrapper').attr('class').indexOf('active');
+                // Make sure the scrolling has passed a certain threshold
+                if (Math.abs(lastScrollTop - windowTop) <= delta) {
                     return;
-                if (windowTop > lastScrollTop && windowTop > navbarHeight) {
-                    $(elem).removeClass('menu-show').addClass('menu-hide');
                 } else {
-                    if (windowTop + $(window).height() < $(document).height()) {
+                    if (windowTop > lastScrollTop && windowTop > navbarHeight && $dropDownsIsActive == -1) {
+                        $(elem).removeClass('menu-show').addClass('menu-hide');
+                    } else if (windowTop + $(window).height() < $(document).height()) {
                         $(elem).removeClass('menu-hide').addClass('menu-show');
                     }
                 }
