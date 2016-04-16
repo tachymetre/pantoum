f66c81b6a12c93c9b93ba26d8def5e7917a75f66
tachymetre <vince.pham.149@gmail.com>
2016-04-16 09:17:09 -0400

    add in database migrations and update css

---
+++ server/database/migrations/2016_04_15_210330_create_highlights_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHighlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('highlights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('tag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('highlights');
+    }
+}
---
+++ server/database/migrations/2016_04_16_035207_add_like_to_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLikeToBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            // $table->integer('like');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('like');
+        });
+    }
+}
