fb79aa7f7c96798661657d1c1743416fc5a5f746
tachymetre <vince.pham.149@gmail.com>
2016-04-20 12:54:33 -0400

    add in util icons for each blog post

--- client/src/scripts/controllers/blogsController.js
+++ client/src/scripts/controllers/blogsController.js
@@ -12,7 +12,6 @@ module.exports = function($http) {
             params: { page: vm.lastPage }
         }).success(function(blogs, status, headers, config) {
             vm.blogs = blogs.data;
-            console.log(vm.blogs);
             vm.currentPage = blogs.current_page;
         });
     };
