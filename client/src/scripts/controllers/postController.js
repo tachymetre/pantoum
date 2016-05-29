6271764de9f13f6bd4afffb71884614019e97455
tachymetre <bminhz@yahoo.com.vn>
2016-05-29 13:31:57 -0400

    add in postController to retrieve each single post

--- client/src/scripts/controllers/postController.js
+++ client/src/scripts/controllers/postController.js
@@ -1,5 +1,11 @@
 'use strict';
-module.exports = function() {
+module.exports = function($stateParams, blogsService) {
     var vm = this;
 
+    vm.init = (function() {
+    	blogsService.getSingleBlog($stateParams.blogId).then(function(response) {
+    		vm.blog = response.data.data;
+    		console.log(vm.blog);
+    	});
+    })();
 }
